76e22fb563224688911dc580517673803183fb01
ZXC <496461440@qq.com>
2018-11-13 23:36:28 +0800

    spring cloud config

--- src/main/java/com/springboot/config/config/SpringCloudConfigApplication.java
+++ src/main/java/com/springboot/config/config/SpringCloudConfigApplication.java
@@ -3,10 +3,12 @@ package com.springboot.config.config;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SpringCloudConfigApplication {
 
     public static void main(String[] args) {
